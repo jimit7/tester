c4fe329e75c39c9794670d281eb6fda24d94b324
Jimit Patel <jimitypatel1995@gmail.com>
2019-07-13 12:30:47 -0400

    tester

--- src/host/libpixyusb2_examples/get_blocks_cpp_demo/get_blocks_cpp_demo.cpp
+++ src/host/libpixyusb2_examples/get_blocks_cpp_demo/get_blocks_cpp_demo.cpp
@@ -13,474 +13,88 @@
 // end license header
 //
 #include <iostream>
-#include <unistd.h>
-
+#include <cstdio>
 #include <signal.h>
 #include "libpixyusb2.h"
-#define XMin 155
-#define XMax 165 
-#include<string>
-#include<fstream>
-#include<sstream>
-#include<cstdlib>
-#include<cstdio>
+#include<stdio.h>
 #include<fcntl.h>
 #include<unistd.h>
-#include<sys/epoll.h>
-#include<pthread.h>
-using std::string;
-using std::ofstream;
-using std::ostringstream;
-#define GPIO_PATH "/sys/class/gpio/"
-
-namespace exploringBB {
-
-typedef int (*CallbackType)(int);
-enum GPIO_DIRECTION{ INPUT, OUTPUT };
-enum GPIO_VALUE{ LOW=0, HIGH=1 };
-enum GPIO_EDGE{ NONE, RISING, FALLING, BOTH };
-
-class GPIO {
-private:
-	int number, debounceTime;
-	string name, path;
-
-public:
-	GPIO(int number); //constructor will export the pin
-	virtual int getNumber() { return number; }
-
-	// General Input and Output Settings
-	virtual int setDirection(GPIO_DIRECTION);
-	virtual GPIO_DIRECTION getDirection();
-	virtual int setValue(GPIO_VALUE);
-	virtual int toggleOutput();
-	virtual GPIO_VALUE getValue();
-	virtual int setActiveLow(bool isLow=true);  //low=1, high=0
-	virtual int setActiveHigh(); //default
-	//software debounce input (ms) - default 0
-	virtual void setDebounceTime(int time) { this->debounceTime = time; }
-
-	// Advanced OUTPUT: Faster write by keeping the stream alive (~20X)
-	virtual int streamOpen();
-	virtual int streamWrite(GPIO_VALUE);
-	virtual int streamClose();
-
-	virtual int toggleOutput(int time); //threaded invert output every X ms.
-	virtual int toggleOutput(int numberOfTimes, int time);
-	virtual void changeToggleTime(int time) { this->togglePeriod = time; }
-	virtual void toggleCancel() { this->threadRunning = false; }
-
-	// Advanced INPUT: Detect input edges; threaded and non-threaded
-	virtual int setEdgeType(GPIO_EDGE);
-	virtual GPIO_EDGE getEdgeType();
-	virtual int waitForEdge(); // waits until button is pressed
-	virtual int waitForEdge(CallbackType callback); // threaded with callback
-	virtual void waitForEdgeCancel() { this->threadRunning = false; }
-
-	virtual ~GPIO();  //destructor will unexport the pin
-
-private:
-	int write(string path, string filename, string value);
-	int write(string path, string filename, int value);
-	string read(string path, string filename);
-//	int exportGPIO();
-//	int unexportGPIO();
-	ofstream stream;
-	pthread_t thread;
-	CallbackType callbackFunction;
-	bool threadRunning;
-	int togglePeriod;  //default 100ms
-	int toggleNumber;  //default -1 (infinite)
-	friend void* threadedPoll(void *value);
-	friend void* threadedToggle(void *value);
+#include<sys/ioctl.h>
+#include<stdint.h>
+#include<linux/spi/spidev.h>
+#define SPI_PATH "/dev/spidev1"
+#define Xmax 175
+#define Xmin 125
+const unsigned char symbols[5] = { 
+1010,0101,1001,0110,0000
 };
 
-void* threadedPoll(void *value);
-void* threadedToggle(void *value);
+int transfer(int fd, unsigned char send[], unsigned char rec[], int len){
+   struct spi_ioc_transfer transfer;        //the transfer structure
+   transfer.tx_buf = (unsigned long) send;  //the buffer for sending data
+   transfer.rx_buf = (unsigned long) rec;   //the buffer for receiving data
+   transfer.len = len;                      //the length of buffer
+   transfer.speed_hz = 1000000;             //the speed in Hz
+   transfer.bits_per_word = 8;              //bits per word
+   transfer.delay_usecs = 0;                //delay in us
+   transfer.cs_change = 0;       // affects chip select after transfer
+   transfer.tx_nbits = 0;        // no. bits for writing (default 0)
+   transfer.rx_nbits = 0;        // no. bits for reading (default 0)
+   transfer.pad = 0;             // interbyte delay - check version
 
-GPIO::GPIO(int number) {
-	this->number = number;
-	this->debounceTime = 0;
-	this->togglePeriod=100;
-	this->toggleNumber=-1; //infinite number
-	this->callbackFunction = NULL;
-	this->threadRunning = false;
-
-	ostringstream s;
-	s << "gpio" << number;
-	this->name = string(s.str());
-	this->path = GPIO_PATH + this->name + "/";
-//	this->exportGPIO();
-	// need to give Linux time to set up the sysfs structure
-	usleep(250000); // 250ms delay
-}/
-
-int GPIO::write(string path, string filename, string value){
-   ofstream fs;
-   fs.open((path + filename).c_str());
-   if (!fs.is_open()){
-	   perror("GPIO: write failed to open file ");
-	   return -1;
+   // send the SPI message (all of the above fields, inc. buffers)
+   int status = ioctl(fd, SPI_IOC_MESSAGE(1), &transfer);
+   if (status < 0) {
+      perror("SPI: SPI_IOC_MESSAGE Failed");
+      return -1;
    }
-   fs << value;
-   fs.close();
-   return 0;
+   return status;
 }
 
-string GPIO::read(string path, string filename){
-   ifstream fs;
-   fs.open((path + filename).c_str());
-   if (!fs.is_open()){
-	   perror("GPIO: read failed to open file ");
-    }
-   string input;
-   getline(fs,input);
-   fs.close();
-   return input;
-}
-
-int GPIO::write(string path, string filename, int value){
-   stringstream s;
-   s << value;
-   return this->write(path,filename,s.str());
-}
-
-//int GPIO::exportGPIO(){
-//   return this->write(GPIO_PATH, "export", this->number);
-//}
-
-//int GPIO::unexportGPIO(){
-//   return this->write(GPIO_PATH, "unexport", this->number);
-//}
-
-int GPIO::setDirection(GPIO_DIRECTION dir){
-   switch(dir){
-   case INPUT: return this->write(this->path, "direction", "in");
-      break;
-   case OUTPUT:return this->write(this->path, "direction", "out");
-      break;
-   }
-   return -1;
-}
-
-int GPIO::setValue(GPIO_VALUE value){
-   switch(value){
-   case HIGH: return this->write(this->path, "value", "1");
-      break;
-   case LOW: return this->write(this->path, "value", "0");
-      break;
-   }
-   return -1;
-}
-
-int GPIO::setEdgeType(GPIO_EDGE value){
-   switch(value){
-   case NONE: return this->write(this->path, "edge", "none");
-      break;
-   case RISING: return this->write(this->path, "edge", "rising");
-      break;
-   case FALLING: return this->write(this->path, "edge", "falling");
-      break;
-   case BOTH: return this->write(this->path, "edge", "both");
-      break;
-   }
-   return -1;
-}
-
-int GPIO::setActiveLow(bool isLow){
-   if(isLow) return this->write(this->path, "active_low", "1");
-   else return this->write(this->path, "active_low", "0");
-}
-
-int GPIO::setActiveHigh(){
-   return this->setActiveLow(false);
-}
-
-GPIO_VALUE GPIO::getValue(){
-	string input = this->read(this->path, "value");
-	if (input == "0") return LOW;
-	else return HIGH;
-}
-
-GPIO_DIRECTION GPIO::getDirection(){
-	string input = this->read(this->path, "direction");
-	if (input == "in") return INPUT;
-	else return OUTPUT;
-}
-
-GPIO_EDGE GPIO::getEdgeType(){
-	string input = this->read(this->path, "edge");
-	if (input == "rising") return RISING;
-	else if (input == "falling") return FALLING;
-	else if (input == "both") return BOTH;
-	else return NONE;
-}
-
-int GPIO::streamOpen(){
-	stream.open((path + "value").c_str());
-	return 0;
-}
-int GPIO::streamWrite(GPIO_VALUE value){
-	stream << value << std::flush;
-	return 0;
-}
-int GPIO::streamClose(){
-	stream.close();
-	return 0;
-}
-
-int GPIO::toggleOutput(){
-	this->setDirection(OUTPUT);
-	if ((bool) this->getValue()) this->setValue(LOW);
-	else this->setValue(HIGH);
-    return 0;
-}
-
-int GPIO::toggleOutput(int time){ return this->toggleOutput(-1, time); }
-int GPIO::toggleOutput(int numberOfTimes, int time){
-	this->setDirection(OUTPUT);
-	this->toggleNumber = numberOfTimes;
-	this->togglePeriod = time;
-	this->threadRunning = true;
-    if(pthread_create(&this->thread, NULL, &threadedToggle, static_cast<void*>(this))){
-    	perror("GPIO: Failed to create the toggle thread");
-    	this->threadRunning = false;
-    	return -1;
-    }
-    return 0;
-}
-
-// This thread function is a friend function of the class
-void* threadedToggle(void *value){
-	GPIO *gpio = static_cast<GPIO*>(value);
-	bool isHigh = (bool) gpio->getValue(); //find current value
-	while(gpio->threadRunning){
-		if (isHigh)	gpio->setValue(HIGH);
-		else gpio->setValue(LOW);
-		usleep(gpio->togglePeriod * 500);
-		isHigh=!isHigh;
-		if(gpio->toggleNumber>0) gpio->toggleNumber--;
-		if(gpio->toggleNumber==0) gpio->threadRunning=false;
-	}
-	return 0;
-}
-
-// Blocking Poll - based on the epoll socket code in the epoll man page
-int GPIO::waitForEdge(){
-	this->setDirection(INPUT); // must be an input pin to poll its value
-	int fd, i, epollfd, count=0;
-	struct epoll_event ev;
-	epollfd = epoll_create(1);
-    if (epollfd == -1) {
-	   perror("GPIO: Failed to create epollfd");
-	   return -1;
-    }
-    if ((fd = open((this->path + "value").c_str(), O_RDONLY | O_NONBLOCK)) == -1) {
-       perror("GPIO: Failed to open file");
-       return -1;
-    }
-
-    //ev.events = read operation | edge triggered | urgent data
-    ev.events = EPOLLIN | EPOLLET | EPOLLPRI;
-    ev.data.fd = fd;  // attach the file file descriptor
-
-    //Register the file descriptor on the epoll instance, see: man epoll_ctl
-    if (epoll_ctl(epollfd, EPOLL_CTL_ADD, fd, &ev) == -1) {
-       perror("GPIO: Failed to add control interface");
-       return -1;
-    }
-	while(count<=1){  // ignore the first trigger
-		i = epoll_wait(epollfd, &ev, 1, -1);
-		if (i==-1){
-			perror("GPIO: Poll Wait fail");
-			count=5; // terminate loop
-		}
-		else {
-			count++; // count the triggers up
-		}
-	}
-    close(fd);
-    if (count==5) return -1;
-	return 0;
-}
-
-// This thread function is a friend function of the class
-void* threadedPoll(void *value){
-	GPIO *gpio = static_cast<GPIO*>(value);
-	while(gpio->threadRunning){
-		gpio->callbackFunction(gpio->waitForEdge());
-		usleep(gpio->debounceTime * 1000);
-	}
-	return 0;
-}
-
-int GPIO::waitForEdge(CallbackType callback){
-	this->threadRunning = true;
-	this->callbackFunction = callback;
-    // create the thread, pass the reference, address of the function and data
-    if(pthread_create(&this->thread, NULL, &threadedPoll, static_cast<void*>(this))){
-    	perror("GPIO: Failed to create the poll thread");
-    	this->threadRunning = false;
-    	return -1;
-    }
-    return 0;
-}
-
-GPIO::~GPIO() {
-//	this->unexportGPIO();
-}
-
-} 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-using namespace std;
-using namespace exploringBB;                           
 Pixy2        pixy;
 static bool  run_flag = true;
 
 
 void handle_SIGINT(int unused)
 {
-
+  // On CTRL+C - abort! //
 
   run_flag = false;
 }
+//void tes_spi()
+//{
 
-void  get_blocks()
-{
-GPIO outGPIO(44),out1GPIO(26),out2GPIO(46),out3GPIO(65);
-outGPIO.setDirection(OUTPUT);
-out1GPIO.setDirection(OUTPUT);
-out2GPIO.setDirection(OUTPUT);
-out3GPIO.setDirection(OUTPUT);
-  int  Block_Index;
-uint16_t x1,y1,w,h;
+//}
+//void  get_blocks()
+//{
+  //int  Block_Index;
 
-  pixy.ccc.getBlocks();
-
+  // Query Pixy for blocks //
+  //pixy.ccc.getBlocks();
  
-  if (pixy.ccc.numBlocks)
-  {
+  // Were blocks detected? //
+  //if (pixy.ccc.numBlocks)
+  //{
+    // Blocks detected - print them! //
 
+    //printf ("Detected %d block(s)\n", pixy.ccc.numBlocks);
 
-    printf ("Detected %d block(s)\n", pixy.ccc.numBlocks);
+    //for (Block_Index = 0; Block_Index < pixy.ccc.numBlocks; ++Block_Index)
+    //{
+      //printf ("  Block %d: ", Block_Index + 1);
+      //pixy.ccc.blocks[Block_Index].print();
 
-    for (Block_Index = 0; Block_Index < pixy.ccc.numBlocks; ++Block_Index)
-    {
-      printf ("  Block %d: ", Block_Index + 1);
-      x1=pixy.ccc.blocks[Block_Index].m_x;
-y1=pixy.ccc.blocks[Block_Index].m_y;
-w=pixy.ccc.blocks[Block_Index].m_width;
-h=pixy.ccc.blocks[Block_Index].m_height;
-printf("x=%d/n",x1);
-printf("y=%d/n",y1);
-printf("w=%d/n",w);
-printf("h=%d/n",h);
+    //}
+  //}
 
-
-if((pixy.ccc.blocks[Block_Index].m_x)<XMin)
-{
-                                                                           	//while(!((pixy.ccc.blocks[Block_Index].m_x)>= XMin && (pixy.ccc.blocks[Block_Index].m_x) <= XMax))
-										//{
-printf("x=%d/n",(pixy.ccc.blocks[Block_Index].m_x));
-printf("left= 0110\n");
-outGPIO.setValue(LOW);
-out1GPIO.setValue(HIGH);
-out2GPIO.setValue(HIGH);
-out3GPIO.setValue(LOW);
 
 }
-                                                                                  	//if(((pixy.ccc.blocks[Block_Index].m_x) >= XMin && (pixy.ccc.blocks[Block_Index].m_x) <= XMax))
-                                                   					//{
-											//break;     
-											//left;
-											//}
-											// printf("In  L Center\n"); 
-											//}
 
-if((pixy.ccc.blocks[Block_Index].m_x)>XMax)
-{
-//while(!((pixy.ccc.blocks[Block_Index].m_x) <= XMax && (pixy.ccc.blocks[Block_Index].m_x) >= XMin))
-//{
-printf("x=%d/n",(pixy.ccc.blocks[Block_Index].m_x));
-printf("RIght= 1001\n");
-
-//if(((pixy.ccc.blocks[Block_Index].m_x) >= XMin && (pixy.ccc.blocks[Block_Index].m_x) <= XMax))
-//{
-//break;
-//left;
-//}
-//}
- printf("In R Center"); 
-outGPIO.setValue(HIGH);
-out1GPIO.setValue(LOW);
-out2GPIO.setValue(LOW);
-out3GPIO.setValue(HIGH);
-}
-if(((pixy.ccc.blocks[Block_Index].m_x)>=XMin && (pixy.ccc.blocks[Block_Index].m_x) <=XMax)){
-printf("x=%d/n",(pixy.ccc.blocks[Block_Index].m_x));
-printf("center= 1010\n");
-outGPIO.setValue(HIGH);
-out1GPIO.setValue(LOW);
-out2GPIO.setValue(HIGH);
-out3GPIO.setValue(LOW);
-//}
-}
-}
-}
-}
 int main()
 {
+   int fd;                              // file handle and loop counter
+   unsigned char null=0x00;                // sending only a single char
+   uint8_t mode = 3;                       // SPI mode 3
   int  Result;
 
   // Catch CTRL+C (SIGINT) signals //
@@ -501,46 +115,117 @@ int main()
       printf ("Error\n");
       printf ("pixy.init() returned %d\n", Result);
       return Result;
-    }
+    	}
 
     printf ("Success\n");
-  }
+  	}
 
   // Get Pixy2 Version information //
-  {
+  	{
     Result = pixy.getVersion();
 
     if (Result < 0)
-    {
+    	{
       printf ("pixy.getVersion() returned %d\n", Result);
       return Result;
-    }
+    	}
 
     pixy.version->print();
-  }
+  	}
 
   // Set Pixy2 to color connected components program //
   pixy.changeProg("color_connected_components");
 
-  while(1)
-  {
-   get_blocks();
-//x1=pixy.ccc.blocks[m_index].m_x;
-//y1=pixy.ccc.blocks[m_index].m_y;
-//w=pixy.ccc.blocks[m_index].m_width;
-//h=pixy.ccc.blocks[m_index].m_height;
-//printf("x=%d/n",x1);
-//printf("y=%d/n",y1);
-//printf("w=%d/n",w);
-//printf("h=%d/n",h);
+  
 
-    if (run_flag == false)
-    {
+while(1)
+  	{
+    	int  Block_Index;
+	// Query Pixy for blocks //
+  	pixy.ccc.getBlocks();
+ 
+ 	 // Were blocks detected? //
+ 	 if (pixy.ccc.numBlocks)
+  		
+    	// Blocks detected - print them! //
+
+   	 printf ("Detected %d block(s)\n", pixy.ccc.numBlocks);
+	
+   	 for (Block_Index = 0; Block_Index < pixy.ccc.numBlocks; ++Block_Index)
+    		{
+      	printf ("  Block %d: ", Block_Index + 1);
+    	 // pixy.ccc.blocks[Block_Index].print();
+	
+  	
+
+ if ((fd = open(SPI_PATH, O_RDWR))<0){
+      perror("SPI Error: Can't open device.");
+      return -1;
+   }
+   if (ioctl(fd, SPI_IOC_WR_MODE, &mode)==-1){
+      perror("SPI: Can't set SPI mode.");
+      return -1;
+   }
+   if (ioctl(fd, SPI_IOC_RD_MODE, &mode)==-1){
+      perror("SPI: Can't get SPI mode.");
+      return -1;
+   }
+
+
+
+
+if((pixy.ccc.blocks[Block_Index].m_x)<XMin)
+	{
+if (transfer(fd, (unsigned char*) &symbols[3], &null, 1)==-1)
+	{
+         perror("Failed to update the display");
+         return -1;
+      	}
+printf("left= 0110\n");
+printf("x=%d/n",(pixy.ccc.blocks[Block_Index].m_x));
+	}
+
+
+
+
+
+if((pixy.ccc.blocks[Block_Index].m_x)>XMax)
+	{
+if (transfer(fd, (unsigned char*) &symbols[2], &null, 1)==-1)
+	{
+         perror("Failed to update the display");
+         return -1;
+      	}
+printf("x=%d/n",(pixy.ccc.blocks[Block_Index].m_x));
+printf("Right= 1001\n");
+	}
+
+
+
+
+if(((pixy.ccc.blocks[Block_Index].m_x)>=XMin && (pixy.ccc.blocks[Block_Index].m_x) <=XMax))
+	{
+	if (transfer(fd, (unsigned char*) &symbols[0], &null, 1)==-1)
+	{
+         perror("Failed to update the display");
+         return -1;
+      	}
+printf("x=%d/n",(pixy.ccc.blocks[Block_Index].m_x));
+printf("forward=1010");
+	}
+
+    
+
+
+
+
+if (run_flag == false)
+    	{
       // Exit program loop //
       break;
-    }
-  }
-
+    	}
+  	}
+ 	close(fd);
   printf ("PIXY2 Get Blocks Demo Exit\n");
-}
-
+	}
+	}
