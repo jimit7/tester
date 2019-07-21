34189009a129f87ea96a576eddf28e64ba11e760
Jimit Patel <40808237+jimit7@users.noreply.github.com>
2019-07-21 15:46:33 -0400

    Update get_blocks_cpp_demo.cpp

--- src/host/libpixyusb2_examples/get_blocks_cpp_demo/get_blocks_cpp_demo.cpp
+++ src/host/libpixyusb2_examples/get_blocks_cpp_demo/get_blocks_cpp_demo.cpp
@@ -134,7 +134,7 @@ if((pixy.ccc.blocks[Block_Index].m_x)<XMin)
  //if ((count = write(sender,"a",2))<0){
    //   perror("UART: Failed to start server 1.\n");
      // return -1;
-   }
+   //}
 printf("left= 0110\n");
 printf("x=%d/n",(pixy.ccc.blocks[Block_Index].m_x));
 	}
@@ -149,7 +149,7 @@ if((pixy.ccc.blocks[Block_Index].m_x)>XMax)
  // if ((count = write(sender,"b",2))<0){
    //   perror("UART: Failed to start server 2.\n");
      // return -1;
-   }
+   //}
      
 printf("x=%d/n",(pixy.ccc.blocks[Block_Index].m_x));
 printf("Right= 1001\n");
@@ -164,7 +164,7 @@ if(((pixy.ccc.blocks[Block_Index].m_x)>=XMin && (pixy.ccc.blocks[Block_Index].m_
 	 //if ((count = write(sender,"c",2))<0){
       //perror("UART: Failed to start server 3.\n");
       //return -1;
-   }
+   //}
 printf("x=%d/n",(pixy.ccc.blocks[Block_Index].m_x));
 printf("forward=1010");
 	}
